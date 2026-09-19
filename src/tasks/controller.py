53268ca1c2460ceba35744c8da96a9bb8bc626a0
Amrita <amritakumari24@navgurukul.org>
2026-09-19 16:01:31 +0530

    getonetask

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -1,6 +1,7 @@
 from src.tasks.dtos import TaskSchema
 from sqlalchemy.orm import Session
 from src.tasks.models import TaskModel
+from fastapi import HTTPException
 
 def create_task(body: TaskSchema, db: Session):
     new_task = TaskModel(**body.model_dump())
@@ -21,4 +22,10 @@ def create_task(body: TaskSchema, db: Session):
 def get_tasks(db:Session ):
     tasks = db.query(TaskModel).all()
     return {"status": "all tasks", "data": tasks}
-    
+
+
+def get_one_task(task_id:int,db:Session):
+    one_task = db.query(TaskModel).get(task_id)
+    if not one_task:
+        raise HTTPException(404, detail="task id is incoorect")
+    return {"status": "task fetched successfully", "data": one_task}
